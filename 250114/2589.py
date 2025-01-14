fd847079b12339bafa69b078511bc16c775dcea2
unknown <20102106@officestu.seoultech.ac.kr>
2025-01-14 23:51:18 +0900

    .

--- 250114/2589.py
+++ 250114/2589.py
@@ -17,20 +17,29 @@ res=0
 
 for i in range(h):
     for j in range(w):
-        queue = deque()
-        visited=[[-1 for _1 in range(w)] for _ in range(h)]
-        visited[i][j]=0
         if world[i][j]=="L":
+            queue = deque()
+            visited=[[-1 for _1 in range(w)] for _ in range(h)]
             queue.append([i, j])
-            
+            cnt=0
+            visited[i][j]=cnt
+            tmplist=deque()
             while queue:
-                nowi, nowj=queue.popleft()
-                for m in move:
-                    tmpi, tmpj=nowi+m[0], nowj+m[1]
-                    if 0<=tmpi<len(world) and 0<=tmpj<len(world[0]) and world[tmpi][tmpj]=="L" and visited[tmpi][tmpj]==-1:
-                        queue.append([tmpi, tmpj])
-                        visited[tmpi][tmpj]=visited[nowi][nowj]+1
-                        if res<visited[tmpi][tmpj]:
-                            res=visited[tmpi][tmpj]
+                flag=False
+                for _ in range(len(queue)):
+                    tmplist.append(queue.popleft())
+                for _2 in range(len(tmplist)):
+                    nowi, nowj=tmplist.popleft()
+                    for m in move:
+                        tmpi, tmpj=nowi+m[0], nowj+m[1]
+                        if 0<=tmpi<len(world) and 0<=tmpj<len(world[0]) and world[tmpi][tmpj]=="L" and visited[tmpi][tmpj]==-1:
+                            queue.append([tmpi, tmpj])
+                            visited[tmpi][tmpj]=cnt
+                            flag=True
+                if flag==True:
+                    cnt+=1
+                    
+            if res<cnt:
+                res=cnt
 
 print(res)
