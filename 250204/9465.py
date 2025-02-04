9249c6ccd3e13692e63f202156d2dbcfc0482b59
unknown <20102106@officestu.seoultech.ac.kr>
2025-02-04 23:40:42 +0900

    .

---
+++ 250204/9465.py
@@ -0,0 +1,22 @@
+import sys
+input=sys.stdin.readline
+
+for _ in range(int(input())):
+    N=int(input())
+    scores=[]
+    for i in range(2):
+        scores.append(list(map(int, input().split())))
+    dp=[[0 for _ in range(N)] for _ in range(2)]
+    for j in range(N):
+        for i in range(2):
+            if j==0:
+                dp[i][j]=scores[i][j]
+            elif j==1:
+                dp[i][j]=scores[i][j]+scores[i-1][j-1]
+            else:
+                dp[i][j]=scores[i][j]+max(dp[i][j-2]+scores[i-1][j-1], dp[i-1][j-2])
+                if dp[i][j-2]+scores[i-1][j-1]==dp[i-1][j-2]:
+                    continue
+        print(dp)
+    print(max(dp[0][N-1], dp[1][N-1]))
+    
