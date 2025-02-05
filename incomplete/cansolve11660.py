04b5714db265c2833b07effcf86a49977138e1f8
unknown <20102106@officestu.seoultech.ac.kr>
2025-02-05 23:16:40 +0900

    .

---
+++ incomplete/cansolve11660.py
@@ -0,0 +1,18 @@
+import sys
+input=sys.stdin.readline
+
+N, M=map(int, input().split())
+matrix=[list(map(int, input().split())) for _ in range(N)]
+
+
+for _ in range(M):
+    x1, y1, x2, y2=map(int, input().split())
+
+    for i in range(x1-1, x2):
+        if i==x1-1:
+            res=matrix[x1-1][y1-1:y2]
+            print(res)
+        else:
+            for j in range(y1-1, y2):
+                res[j-y1+1]+=matrix[i][j]
+    print(sum(res))
