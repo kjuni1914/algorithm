0d894e23841e4e79e2a616f6de0513e23bdf6198
Juni <kjuni1914@gmail.com>
2025-02-06 17:09:23 +0900

    .

--- incomplete/cansolve11660.py
+++ incomplete/cansolve11660.py
@@ -3,7 +3,8 @@ input=sys.stdin.readline
 
 N, M=map(int, input().split())
 matrix=[list(map(int, input().split())) for _ in range(N)]
-
+memoization=set()
+calValue={}
 
 for _ in range(M):
     x1, y1, x2, y2=map(int, input().split())
@@ -11,8 +12,13 @@ for _ in range(M):
     for i in range(x1-1, x2):
         if i==x1-1:
             res=matrix[x1-1][y1-1:y2]
-            print(res)
         else:
             for j in range(y1-1, y2):
-                res[j-y1+1]+=matrix[i][j]
+                if (i, j, x2-1, y2-1) in memoization:
+                    res[j-y1+1]=calValue[(i, j, x2-1, y2-1)]
+                else:
+                    res[j-y1+1]+=matrix[i][j]
+                    memoization.add((x1-1, y1-1, i, j))
+                    calValue[(x1-1, y1-1, i, j)]=res[j-y1+1]
+
     print(sum(res))
