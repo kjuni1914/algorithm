99c812913b91c24353b6ea7205f7277b3d55f202
Juni <145087501+kjuni1914@users.noreply.github.com>
2025-02-09 15:15:03 +0900

    [Gold V] Title: 회의실 배정, Time: 252 ms, Memory: 60360 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,21 @@
+import sys
+
+input=sys.stdin.readline
+using=[]
+
+for i in range(int(input())):
+    using.append(list(map(int, input().split(" "))))
+
+using.sort(key=lambda x:(x[1], x[0]))
+
+now=-1
+# started=-1
+ans=0
+
+for i in using:
+    if now<=i[0]:# and started!=i[0]:
+        now=i[1]
+        # started=i[0]
+        ans+=1
+
+print(ans)
