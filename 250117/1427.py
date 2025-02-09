c55a80adfa8143cb9d911fb811da28b8475951d6
Juni <kjuni1914@gmail.com>
2025-02-09 15:30:22 +0900

    directory refactorization

--- 250117/1427.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-input=sys.stdin.readline
-
-l=" ".join(input()).split(" ")
-l.pop()
-l.sort(reverse=True)
-print("".join(l))
--- 250205/11053.py
+++
@@ -1,9 +0,0 @@
-import sys
-input=sys.stdin.readline
-
-N=int(input())
-nums=list(map(int, input().split()))
-dp=[0]*(max(nums)+1)
-for i in nums:
-    dp[i]=max(dp[:i])+1
-print(max(dp))
