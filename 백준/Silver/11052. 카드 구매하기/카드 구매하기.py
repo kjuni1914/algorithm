7adddc1717a5926e1cbb475a51ec447e0b9f5064
Juni <145087501+kjuni1914@users.noreply.github.com>
2025-02-10 20:57:20 +0900

    [Silver I] Title: 카드 구매하기, Time: 244 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -0,0 +1,15 @@
+import sys
+
+input=sys.stdin.readline
+
+N=int(input())
+
+tmp=list(map(int, input().split()))
+pack={i+1:tmp[i] for i in range(N)}
+dp=[0]*(N+1)
+
+for i in range(1, N+1):
+    for j in range(1, i+1):
+        dp[i]=max(dp[i-j]+pack[j], pack[i], dp[i])
+
+print(dp[N])
