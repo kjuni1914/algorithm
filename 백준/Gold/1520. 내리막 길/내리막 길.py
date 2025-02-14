dd23ce72dbaa0d2bb071871842592047bf10dfcf
Juni <145087501+kjuni1914@users.noreply.github.com>
2025-02-14 10:18:40 +0900

    [Gold III] Title: 내리막 길, Time: 128 ms, Memory: 49772 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,28 +1,25 @@
 import sys
+sys.setrecursionlimit(10**8)
 
 input=sys.stdin.readline
-sys.setrecursionlimit(100000000)
-
-M, N=map(int, input().split())
 
 field=[]
-for _ in range(M):
+M, N=map(int, input().split())
+
+for i in range(M):
     field.append(list(map(int, input().split())))
 
 visited=[[-1 for _ in range(N)] for _ in range(M)]
-def dfs(x ,y):
-    if x==M-1 and y==N-1:
-        return 1
 
-
-    if visited[x][y]==-1:
-        visited[x][y]=0
-        for dx, dy in zip([0, 0, 1, -1], [1, -1, 0, 0]):
-            if 0<=x+dx<M and 0<=y+dy<N:
-                if field[x+dx][y+dy]<field[x][y]:
-                    visited[x][y]+=dfs(x+dx, y+dy)
-    else:
+def dfs(x, y):
+    if x==M-1 and y==N-1: return 1
+    if visited[x][y]!=-1:
         return visited[x][y]
+    visited[x][y]=0
+    for dx, dy in zip([1, -1, 0, 0], [0, 0, 1, -1]):
+        if 0<=x+dx<M and 0<=y+dy<N and field[x][y]>field[x+dx][y+dy]:
+            visited[x][y]+=dfs(x+dx, y+dy)
+    
     return visited[x][y]
 
-print(dfs(0 ,0))
+print(dfs(0, 0))
