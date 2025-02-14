9c3b19bc92b51ee8772c2a8363785a9e5907b9e1
Juni <145087501+kjuni1914@users.noreply.github.com>
2025-02-14 13:26:59 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1140 ms, Memory: 106844 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,19 @@
+import sys
+input=sys.stdin.readline
+
+N, M=map(int, input().split())
+
+matrix=[]
+
+for _ in range(N):
+    matrix.append(list(map(int, input().split())))
+
+dp=[[0]*(N+1) for _ in range(N+1)]
+
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        dp[i][j]+=dp[i][j-1]+dp[i-1][j]+matrix[i-1][j-1]-dp[i-1][j-1]
+
+for _ in range(M):
+    x1, y1, x2, y2=map(int, input().split())
+    print(dp[x2][y2]-dp[x1-1][y2]-dp[x2][y1-1]+dp[x1-1][y1-1])
