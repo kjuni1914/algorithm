970dae19c36375abf4580b2832501e736d5f4842
Juni <145087501+kjuni1914@users.noreply.github.com>
2025-02-14 10:48:25 +0900

    [Gold V] Title: 합분해, Time: 160 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.py
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -1,18 +1,16 @@
 import sys
+input=sys.stdin.readline
 
 N, K=map(int, input().split())
 
-dp=[[1]*(N+1)]
-for _ in range(K-1):
-    dp.append([0]*(N+1))
+dp=[1]*(N+1)
 
 
 for i in range(1, K):
+    tmp=[0]*(N+1)
     for j in range(N+1):
-        tmp=0
-        for k in range(j+1):
-            tmp+=dp[i-1][k]*dp[0][j-k]
-        dp[i][j]=tmp
+        tmpsum=sum(dp[:j+1])
+        tmp[j]=tmpsum
+    dp=tmp
 
-
-print(dp[K-1][N]%1000000000)
+print(dp[N]%1000000000)
