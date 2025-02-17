f7af0a41b1dc64095a1af50196d6c83be5d51fbb
Juni <145087501+kjuni1914@users.noreply.github.com>
2025-02-17 23:12:59 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -4,22 +4,20 @@ input=sys.stdin.readline
 
 N, M=map(int, input().split())
 
-def backtracking(nums, visited, path, j, res=[]):
+def backtracking(path, j, res=[]):
     if len(path)==M:
         res.append(" ".join(path))
+        return
     
     for i in range(j, N):
-        if not visited[i]:
-            visited[i]=True
-            path.append(nums[i])
-            backtracking(nums, visited, path, i)
-            visited[i]=False
-            path.pop()
+        path.append(nums[i])
+        backtracking(path, i+1)
+        path.pop()
     
     return res
 
 nums=[str(i+1) for i in range(N)]
-res=backtracking(nums, [False]*len(nums), [], 0)
+res=backtracking([], 0)
 
 for i in res:
     print(i)
