f48260581942a213e718a2cea90e7d1d81843612
Juni <kjuni1914@gmail.com>
2025-02-18 20:47:20 +0900

    .

--- temporary.py
+++ temporary.py
@@ -1,36 +1,24 @@
 import sys
-
 input=sys.stdin.readline
 
-cogs=[input().rstrip() for _ in range(4)]
+N, M=map(int, input().split())
 
-def turn(cogs, idx, direction, turned=None):
-    if turned is None:
-        turned=set()
-    now=cogs[idx]
-    turned.add(idx)
+nums=list(map(int, input().split()))
 
-    if idx>0:
-        left=cogs[idx-1]
-        if (idx-1) not in turned and left[2]!=now[6]:
-            turn(cogs, idx-1, -1*direction, turned)
-    if idx<3:
-        right=cogs[idx+1]
-        if (idx+1) not in turned and now[2]!=right[6]:
-            turn(cogs, idx+1, -1*direction, turned)
+nums.sort()
 
-    if direction==-1:
-        now=now[1:8]+now[0]
-    elif direction==1:
-        now=now[7]+now[0:7]
-    cogs[idx]=now
+def backt(idx, path, visited=[False]*len(nums)):
+    if len(path)==M:
+        print(" ".join(path))
+    prev=float('inf')
+    for i in range(len(nums)):
+        if not visited[i] and prev!=nums[i]:
+            visited[i]=True
+            path.append(str(nums[i]))
+            backt(i, path, visited)
+            visited[i]=False
+            path.pop()
+            prev=nums[i]
+    return 
 
-for _ in range(int(input())):
-    idx, direction=map(int, input().split())
-    turn(cogs, idx-1, direction)
-
-ans=0
-for i in range(4):
-    if cogs[i][0]=="1":
-        ans+=2**(i)
-print(ans)
+backt(N+1, [])
