4f3a3bf3c02eb3b3b28c590f03257a8b0b809e40
Juni <145087501+kjuni1914@users.noreply.github.com>
2025-02-23 12:15:32 +0900

    [Silver I] Title: 포도주 시식, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -3,14 +3,9 @@ import sys
 input=sys.stdin.readline
 N=int(input())
 dp=[0, 0, 0]
+
 for i in range(N):
     now=int(input())
-    tmp=dp[:]
-
-    tmp[0]=max(dp)
-    tmp[1]=dp[0]+now
-    tmp[2]=dp[1]+now
-    
-    dp=tmp
+    dp=(max(dp), dp[0]+now, dp[1]+now)
 
 print(max(dp))
