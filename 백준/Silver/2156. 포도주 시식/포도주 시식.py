89f36d17ebbf5023c6bc8b1a40d1d5bd170aec8c
Juni <145087501+kjuni1914@users.noreply.github.com>
2025-02-23 12:14:47 +0900

    [Silver I] Title: 포도주 시식, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -6,13 +6,11 @@ dp=[0, 0, 0]
 for i in range(N):
     now=int(input())
     tmp=dp[:]
-    if i==0:
-        tmp[1]+=now
-        tmp[2]+=now
-    else:
-        tmp[0]=max(dp)
-        tmp[1]=dp[0]+now
-        tmp[2]=dp[1]+now
+
+    tmp[0]=max(dp)
+    tmp[1]=dp[0]+now
+    tmp[2]=dp[1]+now
+    
     dp=tmp
 
 print(max(dp))
