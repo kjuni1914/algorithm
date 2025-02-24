4bc1de52092cba655097228a4aa873dc9afd209a
Juni <145087501+kjuni1914@users.noreply.github.com>
2025-02-24 15:20:25 +0900

    [Gold V] Title: 1로 만들기 2, Time: 684 ms, Memory: 79784 KB -BaekjoonHub

--- 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.py
+++ 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.py
@@ -3,33 +3,25 @@ import sys
 input=sys.stdin.readline
 N=int(input())
 
-dp=[None]*max(4, (N+1))
-length=[float('inf')]*max(4, (N+1))
+dp=[float('inf')]*max(2, (N+1))
+dp[1]=0
+path=[0]*max(2, (N+1))
 
-dp[1], dp[2], dp[3]=[1], [2, 1], [3, 1]
-length[1], length[2], length[3]=0, 1, 1
+for i in range(2, N+1):
+    dp[i]=dp[i-1]+1
+    path[i]=i-1
+    
+    if i%2==0 and dp[i//2]+1<dp[i]:
+        dp[i]=dp[i//2]+1
+        path[i]=i//2
+    
+    if i%3==0 and dp[i//3]+1<dp[i]:
+        dp[i]=dp[i//3]+1
+        path[i]=i//3
 
-for i in range(4, N+1):
-    if i%6==0:
-        if length[i//3]>length[i//2]:
-            length[i]=length[i//2]+1
-            dp[i]=[i]+dp[i//2]
-        else:
-            length[i]=length[i//3]+1
-            dp[i]=[i]+dp[i//3]
-    elif i%3==0:
-        length[i]=length[i//3]+1
-        dp[i]=[i]+dp[i//3]
-    elif i%2==0:
-        length[i]=length[i//2]+1
-        dp[i]=[i]+dp[i//2]
-    else:
-        length[i]=length[i-1]+1
-        dp[i]=[i]+dp[i-1]
+print(dp[N])
 
-    if length[i-1]+1<length[i]:
-        length[i]=length[i-1]+1
-        dp[i]=[i]+dp[i-1]
-
-print(length[N])
-print(" ".join([str(i) for i in dp[N]]))
+while N>1:
+    print(N, end=" ")
+    N=path[N]
+print(1, end="\n")
