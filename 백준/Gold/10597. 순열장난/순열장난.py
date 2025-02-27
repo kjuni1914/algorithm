cea2951da2809e0dd4811762867838cc72b08e45
Juni <145087501+kjuni1914@users.noreply.github.com>
2025-02-27 18:09:26 +0900

    [Gold V] Title: 순열장난, Time: 96 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/10597. 순열장난/순열장난.py
+++ 백준/Gold/10597. 순열장난/순열장난.py
@@ -7,23 +7,21 @@ if len(char)>9:
     MAX=(len(char)-9)//2+9
 else:
     MAX=len(char)
-nums=[i+1 for i in range(MAX)]
 
 def backtracking(char, res, visited):
     if len(visited)==MAX:
         print(res)
         sys.exit()
 
-    for i in range(len(nums)):
-        if nums[i] not in visited:
-            if str(nums[i])==char[0]:
-                visited.add(nums[i])
-                backtracking(char[1:], res+str(nums[i])+" ", visited)
-                visited.remove(nums[i])
+    tmp=int(char[0])
+    if tmp not in visited and 0<tmp<=MAX:
+        visited.add(tmp)
+        backtracking(char[1:], res+char[0]+" ", visited)
+        visited.remove(tmp)
+    tmp=int(char[0:2])
+    if len(char)>=2 and 0<tmp<=MAX and tmp not in visited:
+        visited.add(tmp)
+        backtracking(char[2:], res+char[0:2]+" ", visited)
+        visited.remove(tmp)
 
-            elif str(nums[i])==char[0:2]:
-                visited.add(nums[i])
-                backtracking(char[2:], res+str(nums[i])+" ", visited)
-                visited.remove(nums[i])
-
-print(backtracking(char, "", set()))
+backtracking(char, "", set())
