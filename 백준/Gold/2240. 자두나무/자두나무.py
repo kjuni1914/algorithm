6f72b23b61c167051d5158a9dc143b40ed36f1c7
Juni <145087501+kjuni1914@users.noreply.github.com>
2025-03-03 19:18:27 +0900

    [Gold V] Title: 자두나무, Time: 36 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.py
+++ 백준/Gold/2240. 자두나무/자두나무.py
@@ -3,8 +3,8 @@ import sys
 input=sys.stdin.readline
 T, W=map(int, input().split())
 
-dp=[[0 for _ in range(W+1)] for _ in range(T)]
-for i in range(T):
+dp=[[0 for _ in range(W+1)] for _ in range(T+1)]
+for i in range(1, T+1):
     fall=int(input())
     if fall==1:
         dp[i][0]=dp[i-1][0]+1
@@ -16,9 +16,5 @@ for i in range(T):
             dp[i][j]=max(dp[i-1][j], dp[i-1][j-1]+1)
         else:
             dp[i][j]=dp[i-1][j]+1
-
-if T==1:
-    print(1)
-    sys.exit()
 
 print(max(dp[-1]))
