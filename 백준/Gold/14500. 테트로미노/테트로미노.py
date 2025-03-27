baffd34431ae804b44e06b16895049084bf045de
Juni <145087501+kjuni1914@users.noreply.github.com>
2025-03-27 15:24:01 +0900

    [Gold IV] Title: 테트로미노, Time: 1220 ms, Memory: 38328 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -8,52 +8,41 @@ for _ in range(N):
 
 def simul(x, y):
     tmpres=0
-
     if x<N-3:
-        tmpres=max(tmpres, field[x][y]+field[x+1][y]+field[x+2][y]+field[x+3][y])
+        tmpres=field[x][y]+field[x+1][y]+field[x+2][y]+field[x+3][y]
+
     if y<M-3:
         tmpres=max(tmpres, sum(field[x][y:y+4]))
 
-    if x<N-1 and y<M-1:
-        tmpres=max(tmpres, sum(field[x][y:y+2])+sum(field[x+1][y:y+2]))
-
-    if x<N-1 and y<M-2:
-        tmpres=max(tmpres, sum(field[x][y:y+3])+field[x+1][y+1])
-    if 0<x and y<M-2:
-        tmpres=max(tmpres, sum(field[x][y:y+3])+field[x-1][y+1])
     if x<N-2 and y<M-1:
-        tmpres=max(tmpres, field[x][y]+field[x+1][y]+field[x+2][y]+field[x+1][y+1])
-    if x<N-2 and 0<y:
-        tmpres=max(tmpres, field[x][y]+field[x+1][y]+field[x+2][y]+field[x+1][y-1])
-
+        tmp=sum(field[x][y:y+2])+sum(field[x+1][y:y+2])+sum(field[x+2][y:y+2])
+        subtract=[field[x][y]+field[x+2][y+1], 
+                  field[x][y+1]+field[x+2][y], 
+                  field[x][y+1]+field[x+2][y+1], 
+                  field[x][y]+field[x+2][y], 
+                  field[x][y]+field[x+1][y], 
+                  field[x+1][y]+field[x+2][y], 
+                  field[x][y+1]+field[x+1][y+1], 
+                  field[x+1][y+1]+field[x+2][y+1], 
+                  sum(field[x][y:y+2]), 
+                  sum(field[x+2][y:y+2])]
+        for sub in subtract:
+            tmpres=max(tmpres, tmp-sub)
     if x<N-1 and y<M-2:
-        tmpres=max(tmpres, sum(field[x][y:y+2])+sum(field[x+1][y+1:y+3]))
-    if x<N-2 and y<M-1:
-        tmpres=max(tmpres, field[x][y]+sum(field[x+1][y:y+2])+field[x+2][y+1])
-    if 0<x and y<M-2:
-        tmpres=max(tmpres, sum(field[x-1][y+1:y+3])+sum(field[x][y:y+2]))
-    if 1<x and y<M-1:
-        tmpres=max(tmpres, field[x-2][y+1]+sum(field[x-1][y:y+2])+field[x][y])
+        tmp=sum(field[x][y:y+3])+sum(field[x+1][y:y+3])
+        subtract=[field[x][y]+field[x+1][y+2], 
+                  field[x+1][y]+field[x][y+2], 
+                  field[x][y]+field[x][y+2], 
+                  field[x+1][y]+field[x+1][y+2], 
+                  field[x][y]+field[x][y+1], 
+                  field[x][y+1]+field[x][y+2], 
+                  field[x+1][y]+field[x+1][y+1], 
+                  field[x+1][y+1]+field[x+1][y+2], 
+                  field[x][y]+field[x+1][y], 
+                  field[x][y+2]+field[x+1][y+2]]
+        for sub in subtract:
+            tmpres=max(tmpres, tmp-sub)
 
-    if x<N-2 and 0<y:
-        tmpres=max(tmpres, field[x+1][y-1]+field[x][y]+field[x+1][y]+field[x+2][y])
-    if x<N-1 and y<M-2:
-        tmpres=max(tmpres, sum(field[x][y:y+3])+field[x+1][y])
-    if x<N-2 and y<M-1:
-        tmpres=max(tmpres, field[x][y]+field[x+1][y]+field[x+2][y]+field[x][y+1])
-    if 0<x and y<M-2:
-        tmpres=max(tmpres, sum(field[x][y:y+3])+field[x-1][y])
-    if 1<x and y<M-1:
-        tmpres=max(tmpres, field[x-2][y]+field[x-1][y]+field[x][y]+field[x][y+1])
-    if 0<x and 1<y:
-        tmpres=max(tmpres, sum(field[x][y-2:y+1])+field[x-1][y])
-    if 1<x and 0<y:
-        tmpres=max(tmpres, field[x-2][y]+field[x-1][y]+field[x][y]+field[x][y-1])
-    if x<N-1 and 1<y:
-        tmpres=max(tmpres, sum(field[x][y-2:y+1])+field[x+1][y])
-    if x<N-2 and 0<y:
-        tmpres=max(tmpres, field[x][y]+field[x+1][y]+field[x+2][y]+field[x][y-1])
-        
     return tmpres
 
 res=0
