4e22659228bc2c2f45944c5606b284cf121765db
Juni <145087501+kjuni1914@users.noreply.github.com>
2025-03-27 14:01:43 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34944 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,25 @@
+import sys
+from collections import deque
+
+input=sys.stdin.readline
+N, M=map(int, input().split())
+field=[]
+
+for _ in range(N):
+    field.append(list(map(int, (" ".join(input().rstrip())).split())))
+
+def bfs():
+    queue=deque([(0, 0)])
+    visited=[[-1]*M for _ in range(N)]
+    visited[0][0]=1
+
+    while queue:
+        nowx, nowy=queue.popleft()
+        for dx, dy in zip([0, 0, -1, 1], [-1, 1, 0, 0]):
+            tmpx, tmpy=nowx+dx, nowy+dy
+            if 0<=tmpx<N and 0<=tmpy<M and visited[tmpx][tmpy]==-1 and field[tmpx][tmpy]==1:
+                queue.append((tmpx, tmpy))
+                visited[tmpx][tmpy]=visited[nowx][nowy]+1
+    return visited
+
+print(bfs()[-1][-1])
