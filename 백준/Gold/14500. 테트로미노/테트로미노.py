28d39ce1faefaeb58c6112429e36285831f5d9c1
Juni <145087501+kjuni1914@users.noreply.github.com>
2025-03-27 15:25:33 +0900

    [Gold IV] Title: 테트로미노, Time: 1052 ms, Memory: 38328 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -26,8 +26,7 @@ def simul(x, y):
                   field[x+1][y+1]+field[x+2][y+1], 
                   sum(field[x][y:y+2]), 
                   sum(field[x+2][y:y+2])]
-        for sub in subtract:
-            tmpres=max(tmpres, tmp-sub)
+        tmpres=max(tmpres, tmp-min(subtract))
     if x<N-1 and y<M-2:
         tmp=sum(field[x][y:y+3])+sum(field[x+1][y:y+3])
         subtract=[field[x][y]+field[x+1][y+2], 
@@ -40,8 +39,7 @@ def simul(x, y):
                   field[x+1][y+1]+field[x+1][y+2], 
                   field[x][y]+field[x+1][y], 
                   field[x][y+2]+field[x+1][y+2]]
-        for sub in subtract:
-            tmpres=max(tmpres, tmp-sub)
+        tmpres=max(tmpres, tmp-min(subtract))
 
     return tmpres
 
